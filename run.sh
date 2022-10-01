e4af653b7542042cbd6efe95ea2c80bb908d97ac
Steven Zelek <11831150+smzelek@users.noreply.github.com>
2022-10-01 10:48:05 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 full_date=$(date +%F)
 echo "Today is ${full_date}"
 
-git config --global user.name 'Steve Zelek'
-git config --global user.email 'smzelek@gmail.com'
+git config --global user.name '<NAME HERE>'
+git config --global user.email '<EMAIL HERE>'
 git commit --allow-empty -m "${full_date} [skip ci]"
 git push
